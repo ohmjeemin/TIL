9b81e0dcbaac40cfbd36fff4d6abd57b8aab01f7
ohmjeemin <ohmjeemin@gmail.com>
2021-06-30 01:08:18 +0900

    코루틴 이해하기

---
+++ kotlin/kotlin_in_action/src/main/kotlin/coroutine/crpractive.kt
@@ -0,0 +1,23 @@
+package coroutine
+
+
+fun main() {
+
+}
+
+fun drawPerson() {
+   
+    drawHead()
+    drayBody()
+    drawLegs()
+}
+
+suspend fun drawHead() {
+    println(2000)
+}
+suspend fun drayBody() {
+    println(5000)
+}
+suspend fun drawLegs() {
+    println(3000)
+}
--- kotlin/kotlin_in_action/src/main/kotlin/property/Person.kt
+++ kotlin/kotlin_in_action/src/main/kotlin/property/Person.kt
@@ -1,13 +1,36 @@
 package property
 
 class Person private constructor(val name:String, var isMarried:Boolean){
-}
-
-class Animal {
-    val age:Int = 20
-    val name:String ="wong"
-    constructor(age:Int, name:String) {
-
+    init {
+        println("${name}is VIP")
     }
 }
 
+class Animal {
+    var age:Int = 20
+    var name:String ="wong"
+
+    constructor(age:Int, name:String) {
+        this.age = age
+        this.name = name
+    }
+}
+
+class Student(val name:String) {
+    var age:Int
+
+    init {
+        age = 26
+    }
+
+    constructor(name:String, age:Int): this(name){
+        // 이곳에서 init 블럭 실행
+        println(this.age) // 26
+        this.age = age
+        println(this.age) // 27
+    }
+}
+
+fun main(args: Array<String>){
+    val student1 = Student("Jimny", 27)
+}
