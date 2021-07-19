6ea3a3d634742913b7b9ae1d0f5f0e6e3d0f5299
ohmjeemin <ohmjeemin@gmail.com>
2021-07-19 23:56:32 +0900

    두잇 코틀린

---
+++ kotlin/kotlin_in_action/src/main/kotlin/type/anyType.kt
@@ -0,0 +1,20 @@
+package type
+
+fun main() {
+    getArgs("지민", "지현", "규섭", "기율")
+}
+
+fun checkArg(x: Any) {
+    if(x is String) {
+        println("x is string")
+    }
+    if(x is Int) {
+        println("x is Int")
+    }
+}
+
+fun getArgs(vararg names:String){
+    for(name in names){
+        print(name)
+    }
+}
