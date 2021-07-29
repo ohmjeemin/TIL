836955f537b95eee2ad5dfd0984a62e65b556911
ohmjeemin <ohmjeemin@gmail.com>
2021-07-30 00:45:59 +0900

    환경별로 관리하는 .env 파일

--- hello-react/webpack-test2/cra-test/src/App.js
+++ hello-react/webpack-test2/cra-test/src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
+
 
 function App() {
+    console.log(`NODE_ENV = ${process.env.NODE_ENV}`)
   return (
     <div className="App">
+      <TodoList />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
