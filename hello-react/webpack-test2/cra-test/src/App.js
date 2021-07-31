812455aa249448bc0ae5804d088ea0f8274edae1
ohmjeemin <ohmjeemin@gmail.com>
2021-07-31 16:49:30 +0900

    spa 앞부분

--- hello-react/webpack-test2/cra-test/src/App.js
+++ hello-react/webpack-test2/cra-test/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import Button from './Button3';
-import Box from './Box4'
+import React, {useEffect} from 'react';
 
 export default function App() {
+ useEffect(() => {
+     window.onpopstate = function(event) {
+         console.log(`location: ${document.location}, state: ${event.state}`);
+     };
+ }, []);
   return (
       <div>
-          <Button size="big" />
-          <Button size="small" />
-          <Box size="big" />
-          <Box size="small" />
+          <button onClick={() => window.history.pushState('v1', '', '/page1')}>
+              page1
+          </button>
+          <button onClick={() => window.history.pushState('v2', '', '/page2')}>
+              page2
+          </button>
       </div>
   );
 }
