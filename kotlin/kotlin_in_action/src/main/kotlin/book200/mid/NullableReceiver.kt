d715252684db9f1c666381bb969bb43536e3c200
ohmjeemin <ohmjeemin@gmail.com>
2021-08-30 23:56:49 +0900

    Nullable 리시버

---
+++ kotlin/kotlin_in_action/src/main/kotlin/book200/mid/NullableReceiver.kt
@@ -0,0 +1,13 @@
+package book200.mid
+//Nullable 리시버
+//확장함수를 응용하면, 참조 변수에 null이 지정되어 있어도 함수 호출이 가능하게 할 수 있다.
+fun String?.isNumber() {
+    if(this==null){
+        println("문자열이 null입니다")
+    }
+}
+
+fun main(args:Array<String>){
+    val empty:String?=null
+    empty.isNumber()
+}
